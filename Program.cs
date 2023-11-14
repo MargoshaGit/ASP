a0b82209743122ad515f38405691cf40c55a1090
Artem Shvets <fomenkomargo585@gmail.com>
2023-11-15 00:24:13 +0200

    Practice 8

--- Program.cs
+++ Program.cs
@@ -1,35 +1,32 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddMvc();
+
 var app = builder.Build();
 
-Company company = new Company("MyCompany", "The best company ever", 20000000);
 
-app.MapGet("/", () => company.Show());
-app.MapGet("/randomInt", () => "" + new Random().Next(0, 101));
-app.Run();
-
-
-
-
-class Company
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    public string Name { get; set; }
-    public string Description { get; set; }
-    public int Price { get; set; }
-
-    public Company(string name, string description, int price)
-    {
-        this.Name = name;
-        this.Description = description;
-        this.Price = price;
-    }
-    public string Show()
-    {
-        return ("Name of the company: " + this.Name + 
-            "\nCompany information: " + this.Description + 
-            "\nPrice of the company: " + this.Price);
-    }
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    
 }
 
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapControllerRoute(
+    name: "/",
+    pattern: "{controller=Product}/{action=Index}/"
+);
 
 
-
+app.Run();
